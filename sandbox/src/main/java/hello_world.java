4740e7f3e8cd8a914c2d845153eb3daba7716a5b
Shunika <nastashu@gmail.com>
2017-09-25 23:52:30 +0300

    Создан проект Sandbox

---
+++ sandbox/src/main/java/hello_world.java
@@ -0,0 +1,15 @@
+public class hello_world {
+
+
+  public static void main(String[] args) {
+    System.out.println("Hello,world!");
+  }
+
+}
+  public static boolean doubleExpression(double a, double b, double c) {
+  c=a-b ;
+  if (c< 0.0001) {
+    return "true";
+  }
+
+}
